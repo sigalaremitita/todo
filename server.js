2a074e5938d602b37252b0d407318f05883cb328
rmei remi <sigalaremi@gmail.com>
2019-02-10 16:03:40 +0100

    adding infor to my table

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ var express= require('express');
 var app = express();
 var PORT =process.env.PORT || 3000;
 
+var todo= [{
+    id:1,
+ descriptio: 'meet mon for lunch',
+ completed: false
+},
+{
+    id:2,
+    descriptio: 'mgo to the market',
+    completed: false
+},
+{
+    id:3,
+descriptio: 'i am a king',
+completed: true
+}
+];
+
 app.get('/', function(req, res){
     res.send('totdo api');
+});
+app.get('/todos', function(req, res){
+  res.json(todo);
 });
 
 app.listen(PORT, function(){
