b80b3054ea6689f94f201465fbc1a7aaabcf2b31
rmei remi <sigalaremi@gmail.com>
2019-02-11 05:29:55 +0100

    init rebuild

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ var todo= [{
 {
     id:3,
 descriptio: 'i am a king',
-completed: true
-}
-];
+completed: true}];
+
 
 app.get('/', function(req, res){
     res.send('totdo api');
@@ -25,7 +24,21 @@ app.get('/', function(req, res){
 app.get('/todos', function(req, res){
   res.json(todo);
 });
-
+ app.get('/todos/:id', function(req,res){
+     
+    var todoId= req.params.id;
+    var match;
+    todo.forEach(function(tod) {
+        if (todoId==tod.id) {
+           match =tod;
+        }
+    });
+     if(match){
+         res.json(match);
+     }else{res.status(404).send();}
+     
+     res.send('asking '+ req.params.id);
+ });
 app.listen(PORT, function(){
     console.log('express listening on port'+ PORT );    
 });
